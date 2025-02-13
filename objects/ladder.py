34939c5fae6587f795f4db89c30854598990d5f8
julia <160194207+juliaduken@users.noreply.github.com>
2025-02-12 19:01:39 -0500

    Update ladder.py
    ladder glitch

--- objects/ladder.py
+++ objects/ladder.py
@@ -1,7 +1,9 @@
 import pygame
+import os
+from settings import ASSETS_PATH
 
 class Ladder:
-    def __init__(self, x, y, width, height, image_path="../assets/images/ladder.png"):  # Brown color for the ladder
+    def __init__(self, x, y, width, height, image_path="images/ladder.png"):  # Brown color for the ladder
         """
         Initializes a ladder at the specified position with the given dimensions and color.
         :param x: Horizontal position of the ladder.
@@ -11,8 +13,7 @@ class Ladder:
         :param color: Color of the ladder (default is brown).
         """
         self.rect = pygame.Rect(x, y, width, height)
-        self.image = pygame.image.load(image_path)  # Load the ladder image
-        self.image = pygame.transform.scale(self.image, (width, height))  # Scale the image to fit the ladder size
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_PATH, image_path)), (40,100))  # Scale the image to fit the ladder size
 
     def draw(self, screen):
         """
